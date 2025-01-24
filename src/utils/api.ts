516f8ea13c80bd1519249a17f93b72e465452ec8
Gabriel Berlanga <g.berlanga.s@gmail.com>
2025-01-23 23:30:48 -0800

    fix api fetch

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 export const fetchProductDetails = async () => {
-    const response = await fetch('/data.json');
+    const response = await fetch('./data.json');
     if(!response.ok) {
         throw new Error('Failed to fetch data')
     }
